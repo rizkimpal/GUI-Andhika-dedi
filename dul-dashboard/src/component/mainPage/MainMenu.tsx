535aff1a7d32e99a0266dff8af8eb80e6bbe4e88
unknown <rizkifajarkk@gmail.com>
2022-10-28 23:46:55 +0700

    add record util

--- dul-dashboard/src/component/mainPage/MainMenu.tsx
+++ dul-dashboard/src/component/mainPage/MainMenu.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import Recoder from "../utils/Recoder";
 
 const MainMenu = () => {
+  // const [recordFile, setRecordFile] = useState();
+  // const [uploadFile, setUploadFile] = useState();
+
+  // async function RecordFile() {
+  //   try {
+  //   } catch(error) {
+      
+  //   }
+  // }
+  // async function UploadFile() {}
+
   return (
     <div className="grid bg-violet-50 w-screen">
       <div className="grid justify-items-center">
@@ -9,12 +21,13 @@ const MainMenu = () => {
         </div>
       </div>
       <div className=" grid grid-cols-2 justify-items-center text-2xl">
-      <button className="bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">
-        Record langsung
-      </button>
-      <button className="bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">
-        Upload file
-      </button>
+        <button className="bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">
+          Record langsung
+        </button>
+        <button className="bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">
+          Upload file
+        </button>
+        <Recoder />
       </div>
     </div>
   );
--- dul-dashboard/src/component/utils/Recoder.tsx
+++ dul-dashboard/src/component/utils/Recoder.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import vmsg from 'vmsg'
+import React from "react";
+
+import { useReactMediaRecorder } from "react-media-recorder";
 
 const Recoder = () => {
-  return (
-    <div>Recoder</div>
-  )
-}
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ audio: true });
 
-export default Recoder
+  return (
+    <div className=" w-fit h-fit bg-slate-100 flex ">
+      <p>{status}</p>
+      <button onClick={startRecording} className= "bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">Start Recording</button>
+      <button onClick={stopRecording} className= "bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">Stop Recording</button>
+      <audio src={mediaBlobUrl} controls className="" />
+    </div>
+  );
+};
+
+export default Recoder;
