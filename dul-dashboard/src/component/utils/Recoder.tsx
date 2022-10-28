0ddea24560bc4ef38fd4e02ebaea8beee318a94a
unknown <rizkifajarkk@gmail.com>
2022-10-28 23:47:57 +0700

    ketinggalan tadi

--- dul-dashboard/src/component/utils/Recoder.tsx
+++ dul-dashboard/src/component/utils/Recoder.tsx
@@ -7,7 +7,7 @@ const Recoder = () => {
     useReactMediaRecorder({ audio: true });
 
   return (
-    <div className=" w-fit h-fit bg-slate-100 flex ">
+    <div className=" w-fit h-fit bg-slate-100 flex">
       <p>{status}</p>
       <button onClick={startRecording} className= "bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">Start Recording</button>
       <button onClick={stopRecording} className= "bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">Stop Recording</button>
